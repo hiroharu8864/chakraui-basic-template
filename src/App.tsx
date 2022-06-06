348d823b2c2a3d3b955641c2438928c0e6b9525f
Hiroharu Tanaka <hiroharu8864@gol.com>
2022-06-07 08:23:02 +0900

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <h1>Hello CodeSandbox</h1>
+      <h2>Create Chakra UI template</h2>
+      <a href="https://chakra-ui.com/">https://chakra-ui.com/</a>
+      <p>Create Chakra UI Basic Template</p>
+    </div>
+  );
+}
